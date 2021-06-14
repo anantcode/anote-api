b326b7717bde08bdee0bb6a5fd398b885083c3d6
Anant <anantcode@gmail.com>
2021-06-14 19:27:49 +0530

    added register and messages endpoints

--- src/server.js
+++ src/server.js
@@ -1,21 +1,26 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 app.listen(3001);
 
 app.use(express.json());
+app.use(cors());
 
 const dbMock = {
     users: [
         {
             id: "1",
             name: "Anant",
-            email: "ana@gmail.com",
-            password: "test",
+            email: "test1",
+            username: "test1",
+            password: "test1",
         },
         {
             id: "2",
             name: "Anant2",
-            email: "ana2@gmail.com",
+            email: "test2",
+            username: "test2",
             password: "test2",
         },
     ],
@@ -35,11 +40,14 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-    const { email, password } = req.body;
+    handleSignIn(req, res);
+});
 
+const handleSignIn = (req, res) => {
+    const { username, password } = req.body;
     let found = false;
     dbMock.users.forEach((user) => {
-        if (user.email === email && user.password === password) {
+        if (user.username === username && user.password === password) {
             found = true;
             res.json(user);
         }
@@ -47,9 +55,11 @@ app.post("/signin", (req, res) => {
     if (!found) {
         res.json("Not found");
     }
-});
+};
 
 app.get("/write/:id", (req, res) => {
+    // what is done best here?
+
     const { id } = req.params;
     let found = false;
     dbMock.users.forEach((user) => {
@@ -79,7 +89,45 @@ app.post("/write/:id", (req, res) => {
     }
 });
 
+app.post("/register", (req, res) => {
+    const { username, password } = req.body;
+    console.log(`Application to Register: ${username}, ${password}`);
+    let registerSuccess = registerUser(username);
+    if (registerSuccess) {
+        res.json("Successfully registered! ID=" + username);
+    } else {
+        res.status(400).json("Error during registering");
+    }
+});
+
+app.post("/messages", (req, res) => {
+    const { userId, password } = req.body;
+    console.log(`Messages to fetch for userId: ${userId}, ${password}`);
+    let userMsgs = getUserMsgs(userId);
+    if (userMsgs) {
+        console.log(`Successfully retrieved msgs:\n${userMsgs}\n`);
+        res.json(userMsgs);
+    } else {
+        res.status(400).json("Error getting msgs for user.");
+    }
+});
+
+//mock
+const getUserMsgs = (userId) => {
+    let mockMessagesDB = {
+        1: ["Hello you are wulz.", "You are my tonia"],
+        2: ["Alu lelo", "Bhindi lelo"],
+    };
+
+    return mockMessagesDB[userId];
+};
+
 const write = (user, message) => {
     dbMockMessage[user.id].message.push(message);
     console.log(dbMockMessage);
+};
+
+const registerUser = (username) => {
+    console.log("Registered: " + username);
+    return true;
 };
